0be2eeaf65f1dfd995a73706b66942cc13ea79cd
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-05 18:59:07 +0000

    Prevent log in or creation of new user whilen logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  helper_method :current_patient
   helper_method :logged_in?
+  rescue_from ActionController::RedirectBackError, with: :redirect_to_default
 
   def logged_in?
     !!current_user
@@ -23,14 +23,21 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def current_patient
-    @current_patient ||= Patient.find_by(id: params[:id]) if params[:id]
-  end
-
   def admin?
     if current_user && !current_user.admin
         flash[:message] = "You must be a system administrator to access that action."
         redirect_to root_path
     end
+  end
+
+  def already_logged_in?
+    if current_user
+      flash[:message] = "You are already logged in. Please log out if you would like to log in as a new user."
+      redirect_to :back
+    end
+  end
+
+  def redirect_to_default
+    redirect_to root_path
   end
 end
