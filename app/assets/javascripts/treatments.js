a6c027a0a32281ce60244b5a7182720830919fd3
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-08 19:09:14 +0000

    Add ability to render treatment show view with JS

--- app/assets/javascripts/treatments.js
+++ app/assets/javascripts/treatments.js
@@ -10,7 +10,7 @@ const bindEventHandlers = () => {
       url: '/treatments.json'
     })
     .done(function(treatments) {
-      $('.container').html('<ol></ol>')
+      $('.container').html('<div class="notice"></div><h2 class="fh5co-nav center">Treatments</h2><ol></ol>')
       treatments.forEach(treatment => {
         let newTreatment = new Treatment(treatment)
         let treatmentHtml = newTreatment.indexHtml()
@@ -18,7 +18,24 @@ const bindEventHandlers = () => {
       })
     })
   })
+
+  $(document).on('click', '.show_link', function(e) {
+    e.preventDefault()
+    let id = $(this).attr('data-id')
+    $.ajax({
+      method: 'get',
+      url: `/treatments/${id}.json`
+    })
+    .done(function(treatment) {
+      $('.container').html('')
+      let newTreatment = new Treatment(treatment)
+      let treatmentHtml = newTreatment.showHtml()
+      $('.container').append(treatmentHtml)
+    })
+  })
 }
+
+
 
 function Treatment(treatment) {
   this.id = treatment.id
@@ -28,7 +45,16 @@ function Treatment(treatment) {
 
 Treatment.prototype.indexHtml = function() {
   let treatmentHtml = `
-  <li>${this.name} | <a href="/treatments/${this.id}">View Treatment</a></li>
+  <li><a href="/treatments/${this.id}" data-id="${this.id}" class="show_link">${this.name}</a></li>
+  `
+  return treatmentHtml
+}
+
+Treatment.prototype.showHtml = function() {
+  let treatmentHtml = `
+  <div class="notice"></div>
+  <h2 class="center">${this.name}</h2>
+  <p>${this.details}</p>
   `
   return treatmentHtml
 }
